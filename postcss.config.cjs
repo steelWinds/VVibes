5af816378452fd5bd78b2ce13dfc75f031243b54
Kirill <kirillsurov0@gmail.com>
2023-11-25 18:33:44 +0900

    feat: add editorconfig and commitlint

---
+++ postcss.config.cjs
@@ -0,0 +1,26 @@
+const config = {
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': 'postcss-nesting',
+    tailwindcss: {},
+    'postcss-preset-env': {
+      features: { 'nesting-rules': false },
+    },
+    autoprefixer: {},
+    'postcss-pxtorem': {
+      propList: [
+        '*',
+        '!border-left',
+        '!border-right',
+        '!border-top',
+        '!border-bottom',
+        '!border-*',
+        '!border',
+        '!--border-*',
+        '!outline',
+      ],
+    },
+  },
+};
+
+module.exports = config;
