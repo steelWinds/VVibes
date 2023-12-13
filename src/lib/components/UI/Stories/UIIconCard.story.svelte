b09b3484525e0d999b0bf122d1cb16e4f0d30e88
Kirill <kirillsurov0@gmail.com>
2023-12-14 00:04:45 +0900

    fix: add story for ui-icon-card

---
+++ src/lib/components/UI/Stories/UIIconCard.story.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import UIIconCard from '../UIIconCard.svelte'
+	import { FaceMindBlowSolid } from 'flowbite-svelte-icons'
+
+	export let Hst: Hst
+</script>
+
+<Hst.Story>
+	<UIIconCard icon={FaceMindBlowSolid} title="SomeTitle" />
+</Hst.Story>
